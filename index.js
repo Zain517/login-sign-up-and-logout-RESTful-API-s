4a6b6531657000e247d821667fb931c3154850b9
Zain517 <zainulabideen4744@gmail.com>
2021-02-03 12:20:01 +0500

    port change

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('/',function(req,res){
 });
 
 // listening port
-const PORT=process.env.PORT||3000;
+const PORT=process.env.PORT||5000;
 app.listen(PORT,()=>{
     console.log(`app is live at ${PORT}`);
 });
